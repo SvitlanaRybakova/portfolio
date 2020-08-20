982b77a6975dbb17af8403ba03b288d5b1d472a9
Svitlana <rsv0011@gmail.com>
2020-08-20 23:16:31 +0200

    slider connection
    use swiperjs

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
 new WOW().init();
+
+var mySwiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.project-pagination',
+    bulletClass: 'project-bullet',
+    bulletActiveClass: 'project-bullet-active',
+    clickable: true, 
+  },
+  autoplay: {
+    delay: 4000,
+  },
+  effect:'cube',
+ 
+});
+
